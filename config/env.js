16e12660a2fa575c81cdc0a0073893ef0dfa85c5
mmorandini <emmemorandini@gmail.com>
2017-06-21 11:11:48 +0100

    :fire:

--- config/env.js
+++ config/env.js
@@ -1,4 +1,4 @@
 module.exports = {
-  db: process.env.MONGODB_URI || 'mongodb://localhost/wdi-project-2',
+  db: process.env.MONGODB_URI || 'mongodb://127.0.0.1/wdi-project-2',
   port: process.env.PORT || 3000
 };
